9a3cc2313d223378b44cae119bd1e7181d8e01c2
Nada-ibrahim12 <123187920+Nada-ibrahim12@users.noreply.github.com>
2025-09-15 17:14:00 +0300

    add : fix issues + add responsive for all pages

--- js/y-checkout-form.js
+++ js/y-checkout-form.js
@@ -4,7 +4,15 @@ fetch("../../components/y-c-checkout-form.html")
         const form = document.querySelector('[data-y="checkout-form"]');
         if (form) {
             form.innerHTML = data;
+
+            const noteCheckbox = document.getElementById('note-checkbox');
+            const noteText = document.getElementById('note-text');
+
+            if (noteCheckbox && noteText) {
+                noteCheckbox.addEventListener('change', () => {
+                    noteText.style.display = noteCheckbox.checked ? 'block' : 'none';
+                });
+            }
         }
     })
     .catch((err) => console.error("Error loading check form:", err));
-
--- js/y-navbar.js
+++ js/y-navbar.js
@@ -4,6 +4,46 @@ fetch("../../components/y-c-navbar.html")
     const navbar = document.querySelector('[data-y="nav"]');
     if (navbar) {
       navbar.innerHTML = data;
+
+      initNavbarToggle();
     }
   })
   .catch((err) => console.error("Error loading navbar:", err));
+
+function initNavbarToggle() {
+  const toggler = document.querySelector(".y-c-navbar__toggler");
+  const collapse = document.querySelector("#navbarContent");
+  const navLinks = collapse.querySelectorAll("a"); 
+
+  if (!toggler || !collapse) {
+    console.error("Navbar toggler or collapse not found after load!");
+    return;
+  }
+
+  function checkScreen() {
+    if (window.innerWidth < 992) {
+      collapse.style.display = "none";
+    } else {
+      collapse.style.display = "flex";
+    }
+  }
+
+  checkScreen();
+  window.addEventListener("resize", checkScreen);
+
+  toggler.addEventListener("click", function () {
+    if (collapse.style.display === "none" || collapse.style.display === "") {
+      collapse.style.display = "flex";
+    } else {
+      collapse.style.display = "none";
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      if (window.innerWidth < 992) {
+        collapse.style.display = "none";
+      }
+    });
+  });
+}
